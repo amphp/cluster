beef359b33b2b70fc2e00d286306f31eecb12939
Niklas Keller <me@kelunik.com>
2018-03-17 18:38:52 +0100

    Remove outdated test

---
+++ test/DummyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Amp\Cluster\Test;
+
+use Amp\PHPUnit\TestCase;
+
+class DummyTest extends TestCase {
+    public function testDummy() {
+        $this->assertTrue(true);
+    }
+}
